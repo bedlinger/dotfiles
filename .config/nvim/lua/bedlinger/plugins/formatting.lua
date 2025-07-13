1255d558538b632a4b434fb66483889c83fb4dfb
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-07-13 15:04:37 +0200

    add new linters

--- .config/nvim/lua/bedlinger/plugins/formatting.lua
+++ .config/nvim/lua/bedlinger/plugins/formatting.lua
@@ -1,26 +1,45 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      }
-    })
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				json = { "prettierd" },
+				toml = { "taplo" },
+				yaml = { "prettierd" },
+				xml = { "xmlformatter" },
+				vue = { "prettierd" },
+				markdown = { "prettierd" },
+				nginx = { "nginx-config-formatter" },
+				sql = { "sql-formatter" },
+				c_sharp = { "csharpier" },
+				rust = { "rustfmt" },
+				python = { "ruff", "reorder-python-imports", "docformatter" },
+				go = { "golines", "gomodifytags", "gofumpt", "goimports" },
+				java = { "goole-java-format" },
+				kotlin = { "ktfmt" },
+				bash = { "beautysh" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
 
-    vim.keymap.set({ "n", "v" }, "<leader>nv", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
-  end,
+		vim.keymap.set({ "n", "v" }, "<leader>nv", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
