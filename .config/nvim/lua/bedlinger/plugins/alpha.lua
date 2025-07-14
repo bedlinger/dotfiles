e1d6a9de195cfd3023ff0781e4d076ad91adefdd
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-07-14 13:43:15 +0200

    update plugins and add random headers on nvim start

--- .config/nvim/lua/bedlinger/plugins/alpha.lua
+++ .config/nvim/lua/bedlinger/plugins/alpha.lua
@@ -1,20 +1,31 @@
 return {
 	"goolord/alpha-nvim",
+	dependencies = {
+		"MaximilianLloyd/ascii.nvim",
+		"MunifTanjim/nui.nvim",
+	},
 	event = "VimEnter",
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
+		local ascii = require("ascii")
 
-		dashboard.section.header.val = {
-			"                                                     ",
-			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-			"                                                     ",
+		local headers = {
+			ascii.get_random("animals", "dogs"),
+			ascii.get_random("animals", "cats"),
+			ascii.get_random("animals", "pandas"),
+			ascii.get_random("animals", "dinosaurs"),
+			ascii.get_random("movies", "starwars"),
+			ascii.get_random("movies", "toystory"),
+			ascii.get_random("computers", "apple"),
+			ascii.get_random("misc", "skulls"),
+			ascii.get_random("misc", "krakens"),
+			ascii.get_random("text", "neovim"),
+			ascii.get_random("planets", "planets"),
 		}
+		local rand_header = headers[math.random(#headers)]
+
+		dashboard.section.header.val = rand_header
 
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
