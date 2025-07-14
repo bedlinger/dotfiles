efb771ee6dc4f0332a617c1c4f3898f6097d8e59
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-07-14 14:11:01 +0200

    enable inline error messages

--- .config/nvim/lua/bedlinger/plugins/colorscheme.lua
+++ .config/nvim/lua/bedlinger/plugins/colorscheme.lua
@@ -1,13 +1,21 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
-  config = function()
-    require("gruvbox").setup({
-      transparent_mode = true,
-      italic = {
-        strings = false,
-      },
-    })
-    vim.cmd("colorscheme gruvbox")
-  end
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		vim.diagnostic.config({
+			virtual_text = true,
+			underline = true,
+		})
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			command = [[highlight DiagnosticUnderlineError gui=undercurl]],
+			desc = "undercurl errors",
+		})
+		require("gruvbox").setup({
+			transparent_mode = true,
+			italic = {
+				strings = false,
+			},
+		})
+		vim.cmd("colorscheme gruvbox")
+	end,
 }
