b9fe5ddf82bf56b49851f27fd5ee6b904be02217
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-07-15 12:23:34 +0200

    add mapping to open a new terminal tab

--- .config/nvim/lua/bedlinger/core/keymaps.lua
+++ .config/nvim/lua/bedlinger/core/keymaps.lua
@@ -20,3 +20,6 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "<leader>tt", "<cmd>tabnew | terminal<CR>", { desc = "Open new terminal tab" })
+
+keymap.set("t", "<esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
