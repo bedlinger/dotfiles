5641e7f51301abd3dbcf4c85a5bd580a55d7e02a
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-08-03 11:34:09 +0200

    display ip address initialy

--- .config/sketchybar/plugins/network.sh
+++ .config/sketchybar/plugins/network.sh
@@ -3,20 +3,20 @@
 STATE_FILE="/tmp/sketchybar_network_toggle_state"
 
 IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
-SSID=$(ipconfig getsummary en0 | awk -F ' SSID : '  '/ SSID : / {print $2}')
+SSID=$(ipconfig getsummary en0 | awk -F ' SSID : ' '/ SSID : / {print $2}')
 
 if [[ $IP_ADDRESS != "" ]]; then
-        ICON="􀤆"
-        CURRENT_STATE=$(cat "$STATE_FILE" 2>/dev/null || echo "SSID")
+  ICON="􀤆"
+  CURRENT_STATE=$(cat "$STATE_FILE" 2>/dev/null || echo "IP")
 
-        if [ "$CURRENT_STATE" = "SSID" ]; then
-                LABEL="$SSID"
-        else
-                LABEL="$IP_ADDRESS"
-        fi
+  if [ "$CURRENT_STATE" = "SSID" ]; then
+    LABEL="$SSID"
+  else
+    LABEL="$IP_ADDRESS"
+  fi
 else
-        ICON="􁣡"
-        LABEL="Not Connected"
+  ICON="􁣡"
+  LABEL="Not Connected"
 fi
 
 sketchybar --set $NAME icon="$ICON" label="$LABEL"
