5c12be33467959f717c202816cc12a00626f1ee1
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-08-06 23:00:12 +0200

    fix decrement number

--- .config/nvim/lua/bedlinger/core/keymaps.lua
+++ .config/nvim/lua/bedlinger/core/keymaps.lua
@@ -6,7 +6,7 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-keymap.set("n", "<leader>-", "<C-a>", { desc = "Decrement number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
