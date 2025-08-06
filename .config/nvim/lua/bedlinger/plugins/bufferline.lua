4bc6000c930b1d757b4126ebc5b8c4a3f36f447c
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-08-06 23:14:52 +0200

    edit bufferline

--- .config/nvim/lua/bedlinger/plugins/bufferline.lua
+++ .config/nvim/lua/bedlinger/plugins/bufferline.lua
@@ -1,11 +1,19 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      seperator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "tabs",
+			seperator_style = "thick",
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					highlight = "Directory",
+					separator = true, -- use a "true" to enable the default, or set your own character
+				},
+			},
+		},
+	},
 }
