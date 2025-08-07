8582a24eb0331bd708ced9c4b21b309592813d0f
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-08-07 17:22:49 +0200

    add keymap to move tabs

--- .config/nvim/lua/bedlinger/core/keymaps.lua
+++ .config/nvim/lua/bedlinger/core/keymaps.lua
@@ -23,6 +23,8 @@ keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>t+", "<cmd>tabmove+<CR>", { desc = "Move tab to the right" })
+keymap.set("n", "<leader>t-", "<cmd>tabmove-<CR>", { desc = "Move tab to the left" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 keymap.set("n", "<leader>tt", "<cmd>tabnew | terminal<CR>", { desc = "Open new terminal tab" })
 
