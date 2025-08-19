f64dc2ec15b9b6fad624f72666697e759ad048d3
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2025-08-19 13:39:32 +0200

    upate network plugin

--- .config/sketchybar/plugins/network.sh
+++ .config/sketchybar/plugins/network.sh
@@ -1,22 +1,39 @@
 #!/bin/sh
 
 STATE_FILE="/tmp/sketchybar_network_toggle_state"
+ICON="􀤆"
+CURRENT_STATE=$(cat "$STATE_FILE" 2>/dev/null || echo "SSID")
 
-IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
-SSID=$(ipconfig getsummary en0 | awk -F ' SSID : ' '/ SSID : / {print $2}')
+get_ssid() {
+  system_profiler SPAirPortDataType | awk '/Current Network/ {getline;$1=$1; gsub(":",""); print;exit}'
+}
 
-if [[ $IP_ADDRESS != "" ]]; then
-  ICON="􀤆"
-  CURRENT_STATE=$(cat "$STATE_FILE" 2>/dev/null || echo "IP")
+get_ip() {
+  scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1
+}
 
-  if [ "$CURRENT_STATE" = "SSID" ]; then
+if [ "$CURRENT_STATE" = "SSID" ]; then
+  SSID=$(get_ssid)
+
+  if [ -n "$SSID" ]; then
     LABEL="$SSID"
   else
-    LABEL="$IP_ADDRESS"
+    IP_ADDRESS=$(get_ip)
+    if [ -n "$IP_ADDRESS" ]; then
+      LABEL="$IP_ADDRESS"
+    else
+      ICON="􁣡"
+      LABEL="No Connection"
+    fi
   fi
 else
-  ICON="􁣡"
-  LABEL="Not Connected"
+  IP_ADDRESS=$(get_ip)
+  if [ -n "$IP_ADDRESS" ]; then
+    LABEL="$IP_ADDRESS"
+  else
+    ICON="􁣡"
+    LABEL="No Connection"
+  fi
 fi
 
-sketchybar --set $NAME icon="$ICON" label="$LABEL"
+sketchybar --set "$NAME" icon="$ICON" label="$LABEL"
--- .config/sketchybar/plugins/network_click.sh
+++ .config/sketchybar/plugins/network_click.sh
@@ -5,9 +5,9 @@ STATE_FILE="/tmp/sketchybar_network_toggle_state"
 CURRENT_STATE=$(cat "$STATE_FILE" 2>/dev/null || echo "IP")
 
 if [ "$CURRENT_STATE" = "IP" ]; then
-  echo "SSID" > "$STATE_FILE"
+  echo "SSID" >"$STATE_FILE"
 else
-  echo "IP" > "$STATE_FILE"
+  echo "IP" >"$STATE_FILE"
 fi
 
 sketchybar --trigger network_update
