cf6409db31385f1d9c74e9ac68cbb46615ba5d2d
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2025-12-21 09:34:16 +0100

    fix linting issue with go

--- .config/nvim/lua/bedlinger/plugins/lint.lua
+++ .config/nvim/lua/bedlinger/plugins/lint.lua
@@ -5,6 +5,7 @@ return {
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			local lint = require("lint")
+
 			lint.linters_by_ft = {
 				markdown = { "markdownlint" },
 				javascript = { "eslint_d" },
@@ -18,49 +19,40 @@ return {
 				go = { "golangci-lint" },
 			}
 
-			-- To allow other plugins to add linters to require('lint').linters_by_ft,
-			-- instead set linters_by_ft like this:
-			-- lint.linters_by_ft = lint.linters_by_ft or {}
-			-- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-			--
-			-- However, note that this will enable a set of default linters,
-			-- which will cause errors unless these tools are available:
-			-- {
-			--   clojure = { "clj-kondo" },
-			--   dockerfile = { "hadolint" },
-			--   inko = { "inko" },
-			--   janet = { "janet" },
-			--   json = { "jsonlint" },
-			--   markdown = { "vale" },
-			--   rst = { "vale" },
-			--   ruby = { "ruby" },
-			--   terraform = { "tflint" },
-			--   text = { "vale" }
-			-- }
-			--
-			-- You can disable the default linters by setting their filetypes to nil:
-			-- lint.linters_by_ft['clojure'] = nil
-			-- lint.linters_by_ft['dockerfile'] = nil
-			-- lint.linters_by_ft['inko'] = nil
-			-- lint.linters_by_ft['janet'] = nil
-			-- lint.linters_by_ft['json'] = nil
-			-- lint.linters_by_ft['markdown'] = nil
-			-- lint.linters_by_ft['rst'] = nil
-			-- lint.linters_by_ft['ruby'] = nil
-			-- lint.linters_by_ft['terraform'] = nil
-			-- lint.linters_by_ft['text'] = nil
+			lint.linters.golangci_lint = {
+				name = "golangci-lint",
+				cmd = "golangci-lint",
+				stdin = true,
+				args = {
+					"run",
+					"--out-format",
+					"json",
+					"--show-stats=false",
+					"--print-issued-lines=false",
+					"--print-linter-name=false",
+				},
+				ignore_exitcode = true,
+				parser = require("lint.parser").from_errorformat("%f:%l:%c:%m", {
+					source = "golangci-lint",
+				}),
+			}
 
-			-- Create autocommand which carries out the actual linting
-			-- on the specified events.
 			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 				group = lint_augroup,
 				callback = function()
-					-- Only run the linter in buffers that you can modify in order to
-					-- avoid superfluous noise, notably within the handy LSP pop-ups that
-					-- describe the hovered symbol using Markdown.
-					if vim.bo.modifiable then
+					if not vim.bo.modifiable then
+						return
+					end
+
+					local status, err = pcall(function()
 						lint.try_lint()
+					end)
+
+					if not status then
+						vim.defer_fn(function()
+							print("Linter error: " .. err)
+						end, 0)
 					end
 				end,
 			})
