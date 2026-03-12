13bb1f88b4b8bfe64bd5f46d9324923555a9929c
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2026-03-12 14:52:49 +0100

    display when recording lualine

--- .config/nvim/lua/bedlinger/plugins/lualine.lua
+++ .config/nvim/lua/bedlinger/plugins/lualine.lua
@@ -36,7 +36,18 @@ return {
 				},
 			},
 			sections = {
-				lualine_a = { "mode" },
+				lualine_a = {
+					"mode",
+					{
+						function()
+							local reg = vim.fn.reg_recording()
+							if reg == "" then
+								return ""
+							end -- not recording
+							return "recording to " .. reg
+						end,
+					},
+				},
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
 				lualine_x = { "encoding", "filetype", "lsp_status" },
