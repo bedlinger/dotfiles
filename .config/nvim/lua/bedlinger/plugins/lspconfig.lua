db12eff42bd900cb42e3632ce99094b8c6fe6080
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2026-03-13 08:45:08 +0100

    edit vue config

--- .config/nvim/lua/bedlinger/plugins/lspconfig.lua
+++ .config/nvim/lua/bedlinger/plugins/lspconfig.lua
@@ -264,6 +264,44 @@ return {
 				rust_analyzer = {},
 			}
 
+			local vue_language_server_path =
+				"/Users/bedlinger/.nvm/versions/node/v24.12.0/lib/node_modules/@vue/language-server"
+			local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+			local vue_plugin = {
+				name = "@vue/typescript-plugin",
+				location = vue_language_server_path,
+				languages = { "vue" },
+				configNamespace = "typescript",
+			}
+			local vtsls_config = {
+				settings = {
+					vtsls = {
+						tsserver = {
+							globalPlugins = {
+								vue_plugin,
+							},
+						},
+					},
+				},
+				filetypes = tsserver_filetypes,
+			}
+
+			local ts_ls_config = {
+				init_options = {
+					plugins = {
+						vue_plugin,
+					},
+				},
+				filetypes = tsserver_filetypes,
+			}
+
+			local vue_ls_config = {}
+
+			vim.lsp.config("vtsls", vtsls_config)
+			vim.lsp.config("vue_ls", vue_ls_config)
+			vim.lsp.config("ts_ls", ts_ls_config)
+			vim.lsp.enable({ "vtsls", "vue_ls" })
+
 			-- Ensure the servers and tools above are installed
 			--
 			-- To check the current status of installed tools and/or manually install
