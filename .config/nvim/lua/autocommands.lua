100c5d755beb202ac72cce6884a62b7a05cdde2e
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2026-03-17 08:52:56 +0100

    move autocommand for lsp actions

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -16,3 +16,60 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		require("conform").format({ lsp_format = "fallback" })
 	end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
+		end
+
+		map("grn", vim.lsp.buf.rename, "Rename")
+		map("gra", vim.lsp.buf.code_action, "Goto Code Action", { "n", "x" })
+		map("grr", require("telescope.builtin").lsp_references, "Goto References")
+		map("gri", require("telescope.builtin").lsp_implementations, "Goto Implementation")
+		map("grd", require("telescope.builtin").lsp_definitions, "Goto Definition")
+		map("grD", vim.lsp.buf.declaration, "Goto Declaration")
+		map("grt", require("telescope.builtin").lsp_type_definitions, "Goto Type Definition")
+
+		local function client_supports_method(client, method, bufnr)
+			if vim.fn.has("nvim-0.11") == 1 then
+				return client:supports_method(method, bufnr)
+			else
+				return client.supports_method(method, { bufnr = bufnr })
+			end
+		end
+
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if
+			client
+			and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
+		then
+			local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.document_highlight,
+			})
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.clear_references,
+			})
+			vim.api.nvim_create_autocmd("LspDetach", {
+				group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+				callback = function(event2)
+					vim.lsp.buf.clear_references()
+					vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+				end,
+			})
+		end
+
+		if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+			map("<leader>h", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+			end, "Toggle Inlay Hints")
+		end
+	end,
+})
