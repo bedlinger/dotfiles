feb6293738efbfd8904c7a6a9c7c08ba147bd6f3
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2026-03-22 12:03:45 +0100

    fix name of function

--- .config/sketchybar/items/battery.lua
+++ .config/sketchybar/items/battery.lua
@@ -9,7 +9,7 @@ local function is_battery_charging()
 	return util.execute("pmset -g batt | grep 'AC Power'") ~= ""
 end
 
-local function getBatteryIcon()
+local function get_battery_icon()
 	if is_battery_charging() then
 		return "􀢋"
 	elseif get_battery_percentage() >= 90 then
@@ -30,7 +30,7 @@ end
 local battery = sbar.add("item", {
 	position = "right",
 	label = tostring(get_battery_percentage()) .. "%",
-	icon = getBatteryIcon(),
+	icon = get_battery_icon(),
 	background = { color = colors.muted_yellow },
 	update_freq = 30,
 })
@@ -38,13 +38,13 @@ local battery = sbar.add("item", {
 battery:subscribe("power_source_change", function(_)
 	battery:set({
 		label = tostring(get_battery_percentage()) .. "%",
-		icon = getBatteryIcon(),
+		icon = get_battery_icon(),
 	})
 end)
 
 battery:subscribe("routine", function(_)
 	battery:set({
 		label = tostring(get_battery_percentage()) .. "%",
-		icon = getBatteryIcon(),
+		icon = get_battery_icon(),
 	})
 end)
