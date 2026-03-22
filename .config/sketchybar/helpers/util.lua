2b4ca838abe788bcb9f945b492859419cc326af5
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2026-03-22 12:28:21 +0100

    add util function to convert a string to an array

--- .config/sketchybar/helpers/util.lua
+++ .config/sketchybar/helpers/util.lua
@@ -15,4 +15,12 @@ local function get_display_count()
 	return tonumber(execute("sketchybar --query displays | jq 'length'"))
 end
 
-return { execute = execute, get_display_count = get_display_count }
+local function split(str, sep)
+	local result = {}
+	for s in str:gmatch("[^" .. sep .. "]+") do
+		table.insert(result, s)
+	end
+	return result
+end
+
+return { execute = execute, get_display_count = get_display_count, split = split }
