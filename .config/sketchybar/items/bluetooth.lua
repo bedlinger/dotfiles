86495748e8b7233ab15698eda672de6c1c84db31
Benjamin Edlinger <benjamin.edlinger+github@proton.me>
2026-04-01 11:33:57 +0200

    add indicator when bluetooth is off to item and activate on click

--- .config/sketchybar/items/bluetooth.lua
+++ .config/sketchybar/items/bluetooth.lua
@@ -2,12 +2,21 @@ local colors = require("colors")
 local icons = require("icons")
 local util = require("helpers.util")
 
+local function is_bluetooth_activated()
+	return tonumber(util.execute("blueutil --power")) == 1
+end
+
+local function activate_bluetooth()
+	util.execute("blueutil --power on")
+end
+
 local function is_bluetooth_device_connected()
 	return tostring(util.execute("blueutil --connected")):find("connected") ~= nil
 end
 
 local function get_bluetooth_icon()
-	return is_bluetooth_device_connected() and icons.bluetooth.connected or icons.bluetooth.not_connected
+	return is_bluetooth_activated() and is_bluetooth_device_connected() and icons.bluetooth.connected
+		or icons.bluetooth.not_connected
 end
 
 local bluetooth = sbar.add("item", "bluetooth", {
@@ -15,17 +24,22 @@ local bluetooth = sbar.add("item", "bluetooth", {
 	display = "active",
 	label = { drawing = false },
 	icon = {
-		string = get_bluetooth_icon(),
 		padding_right = 5,
 		padding_left = 5,
 	},
-	background = { color = colors.blue },
 	popup = {
 		align = "right",
 		background = { border_color = colors.blue },
 	},
 	update_freq = 10,
 })
+
+local function set_bluetooth_item_state()
+	bluetooth:set({
+		icon = get_bluetooth_icon(),
+		background = { color = is_bluetooth_activated() and colors.blue or colors.bright_red },
+	})
+end
 
 local function toggle_bluetooth_popup()
 	sbar.exec("sketchybar --remove '/bluetooth.device\\..*/'")
@@ -77,15 +91,17 @@ local function toggle_bluetooth_popup()
 end
 
 bluetooth:subscribe("mouse.clicked", function(_)
-	toggle_bluetooth_popup()
+	if is_bluetooth_activated() then
+		toggle_bluetooth_popup()
+	else
+		activate_bluetooth()
+	end
 end)
 
 bluetooth:subscribe("mouse.exited.global", function(_)
 	bluetooth:set({ popup = { drawing = false } })
 end)
 
-bluetooth:subscribe("routine", function(_)
-	bluetooth:set({
-		icon = get_bluetooth_icon(),
-	})
-end)
+bluetooth:subscribe("routine", set_bluetooth_item_state)
+
+set_bluetooth_item_state()
